d8710359128d8d28c277dc68da0a54d98c8f0209
Gabriel Leclerc <gabriel.leclerc.2@ulaval.ca>
2023-03-29 08:29:22 -0400

    🎉 Finish `progressbar` component

--- mini-component-library/src/components/ProgressBar/ProgressBar.js
+++ mini-component-library/src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,61 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    borderRadius: '4px',
+    height: '8px',
+    padding: '0px',
+  },
+  medium: {
+    borderRadius: '4px',
+    height: '12px',
+    padding: '0px',
+  },
+  large: {
+    borderRadius: '8px',
+    height: '24px',
+    padding: '4px',
+  }
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper size={size}>
+      <BarClipper>
+        <Bar value={value}></Bar>
+        <VisuallyHidden>Progress at {value} %</VisuallyHidden>
+      </BarClipper>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  width: 370px;
+  height: ${props => SIZES[props.size].height};
+  background-color: ${COLORS.transparentGray15};
+  border-radius: ${props => SIZES[props.size].borderRadius};
+  padding: ${props => SIZES[props.size].padding};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  overflow: hidden;
+`;
+
+const BarClipper = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+  background-color: transparent;
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  width: ${props => props.value}%;
+  height: 100%;
+`;
+
+const SmallWrapper = styled(Wrapper)`
+  padding: 0px;
+`;
 
 export default ProgressBar;

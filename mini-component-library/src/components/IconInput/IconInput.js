05555d661a834f52a5c212d49e89a2ab3aad9f5e
Gabriel Leclerc <gabriel.leclerc.2@ulaval.ca>
2023-03-30 22:39:09 -0400

    🎉 Finish workshop

--- mini-component-library/src/components/IconInput/IconInput.js
+++ mini-component-library/src/components/IconInput/IconInput.js
@@ -6,6 +6,17 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    fontSize: 14,
+    lineWidth: 1,
+  },
+  large: {
+    fontSize: 18,
+    lineWidth: 2,
+  },
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +24,58 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+
+  const styles = SIZES[size];
+
+  return (
+    <Wrapper style={{ '--width': width + 'px', '--line-width': styles.lineWidth + 'px'}}>
+      <InputText style={{ '--font-size': (styles.fontSize / 16) + 'rem' }} placeholder={placeholder} />
+      <IconWrapper>
+        <Icon id={icon} size={styles.fontSize * 1.125 + 'px'} strokeWidth={1} />
+      </IconWrapper>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  width: var(--width);
+  border-bottom: var(--line-width) solid ${COLORS.black};
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  margin-top: auto;
+  margin-bottom: auto;
+  height: 1rem;
+  pointer-events: none;
+`;
+
+const InputText = styled.input`
+  font-size: var(--font-size);
+  padding: 4px;
+  padding-left: 28px;
+  border: none;
+  width: inherit;
+  color: inherit;
+  font-weight: 700;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+
+  &:focus {
+    outline-offset: 4px;
+  }
+`;
 
 export default IconInput;

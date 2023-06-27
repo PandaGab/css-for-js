7f55ee15274e40ff66227932b4f9b71403ba4607
PandaGab <gabriel.leclerc.2@ulaval.ca>
2023-06-26 22:03:16 -0400

    WIP

---
+++ sole-and-ankle-revisited/src/constants.js
@@ -0,0 +1,32 @@
+export const COLORS = {
+  white: 'hsl(0deg 0% 100%)',
+  gray: {
+    100: 'hsl(185deg 5% 95%)',
+    300: 'hsl(190deg 5% 80%)',
+    500: 'hsl(196deg 4% 60%)',
+    700: 'hsl(220deg 5% 40%)',
+    900: 'hsl(220deg 3% 20%)',
+  },
+  primary: 'hsl(340deg 65% 47%)',
+  secondary: 'hsl(240deg 60% 63%)',
+};
+
+export const WEIGHTS = {
+  normal: 500,
+  medium: 600,
+  bold: 800,
+};
+
+// Values in pixels:
+const BREAKPOINTS = {
+  phone: 600,
+  tablet: 950,
+  laptop: 1300,
+}
+
+// Converted to rems:
+export const QUERIES = {
+  'tabletAndUp': `(min-width: ${BREAKPOINTS.tabletMin / 16}rem)`,
+  'laptopAndUp': `(min-width: ${BREAKPOINTS.laptopMin / 16}rem)`,
+  'desktopAndUp': `(min-width: ${BREAKPOINTS.desktopMin / 16}rem)`,
+}
